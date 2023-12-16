7999132a385f5839d0193bc6fde30f87d20aaa18
AbdullahEren <a.eyurek@hotmail.com>
2023-12-16 18:33:09 +0300

    Adding Models

---
+++ Entities/Models/ApplicationUser.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+
+        [Required(ErrorMessage = "Name is required.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Username is required.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Address is required.")]
+        public Address Address { get; set; }
+
+        [Required(ErrorMessage = "Phone is required.")]
+        [Phone(ErrorMessage = "Invalid Phone Number.")]
+        public string Phone { get; set; }
+
+        [Url(ErrorMessage = "Invalid Website Url.")]
+        public string Website { get; set; }
+
+        [Required(ErrorMessage = "Company Id is required.")]
+        [ForeignKey(nameof(Company))]
+        public int CompanyId { get; set; }
+
+        [Required(ErrorMessage = "Company is required.")]
+        
+        public Company Company { get; set; }
+
+    }
+
+    public class Address
+    {
+        [Required(ErrorMessage = "Street is required.")]
+        public string Street { get; set; }
+
+        [Required(ErrorMessage = "Suite is required.")]
+        public string Suite { get; set; }
+
+        [Required(ErrorMessage = "City is required.")]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "Zip Code is required.")]
+        public string ZipCode { get; set; }
+
+        [Required(ErrorMessage = "Geo Location is required.")]
+        public GeoLocation Geo { get; set; }
+    }
+
+    public class GeoLocation
+    {
+        [Required(ErrorMessage = "Latitude is required.")]
+        public decimal Lat { get; set; }
+
+        [Required(ErrorMessage = "Longtitude is required.")]
+        public decimal Lng { get; set; }
+    }
+}
---
+++ Entities/Models/Company.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+    public class Company
+    {
+        public int CompanyId { get; set; }
+
+        [Required(ErrorMessage = "Company Name is required.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Catch Phrase is required.")]
+        public string CatchPhrase { get; set; }
+
+        [Required(ErrorMessage = "Business Description is required.")]
+        public string Bs { get; set; }
+
+        [JsonIgnore]
+        public ICollection<ApplicationUser> ApplicationUsers { get; set; }
+
+    }
+}
