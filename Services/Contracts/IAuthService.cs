7a059100c95f7e224c16961609ef8c34ff66ec45
AbdullahEren <a.eyurek@hotmail.com>
2023-12-17 17:19:55 +0300

    Creating Services Contracts

---
+++ Services/Contracts/IAuthService.cs
@@ -0,0 +1,23 @@
+﻿using Entities.Models;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Contracts
+{
+    public interface IAuthService
+    {
+        Task<string> Login(string username, string password);
+        Task<string> Register(string username, string password);
+        IEnumerable<ApplicationRole> Roles { get; }
+        IEnumerable<ApplicationUser> Users { get; }
+        Task<IEnumerable<ApplicationUser>> GetOneUser(string userName);
+        Task<IEnumerable<ApplicationUser>> GetAllUsers();
+        Task<IdentityResult> CreateUser(ApplicationUser userDto);
+        Task<IdentityResult> DeleteOneUser(string userName);
+
+    }
+}
