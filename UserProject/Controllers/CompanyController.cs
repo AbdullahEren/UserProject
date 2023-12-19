71f73b7bef3bf5b35fbd2ea6092e801abefdebb7
AbdullahEren <a.eyurek@hotmail.com>
2023-12-19 15:14:50 +0300

    Creating Company Controller

---
+++ UserProject/Controllers/CompanyController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Entities.Dtos.CompanyDto;
+using Entities.Models;
+using Services.Contracts;
+
+namespace UserProject.Controllers
+{
+    [Route("api/companies")]
+    [ApiController]
+    public class CompanyController : ControllerBase
+    {
+        private readonly ICompanyService _companyService;
+
+        public CompanyController(ICompanyService companyService)
+        {
+            _companyService = companyService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Company>>> GetCompanies()
+        {
+            var companies = await _companyService.GetAllCompaniesAsync(trackChanges: false);
+            return Ok(companies);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Company>> GetCompany(int id)
+        {
+            var company = await _companyService.GetCompanyAsync(id, trackChanges: false);
+            return Ok(company);         
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCompany([FromBody] CompanyForCreationDto companyDto)
+        {
+            await _companyService.CreateCompanyAsync(companyDto);
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateCompany(int id, [FromBody] CompanyForUpdateDto companyDto)
+        {
+            await _companyService.UpdateCompanyAsync(id, companyDto);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCompany(int id)
+        {
+            var company = await _companyService.GetCompanyAsync(id, trackChanges: false);
+            await _companyService.DeleteCompanyAsync(id, company);
+            return Ok();
+        }
+    }
+}
